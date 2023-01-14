e059d1cd0e8a8e5de68352812002a3f1eaeedac5
pierre visconti <94921305+petrosvisconte@users.noreply.github.com>
2023-01-14 08:52:53 -0800

    update

--- install.bash
+++ install.bash
@@ -1,7 +1,14 @@
 #! /bin/bash 
 
 FILE=~/vim-configuration
-if [! -d "$FILE"]; then
+if [ ! -d "$FILE" ]; then
 	echo "$FILE does not exist"
 fi
-
+FILE=~/vim-configuration/colors
+if [ ! -d "$FILE" ]; then
+	echo "$FILE does not exist"
+fi
+FILE=~/vim-configuration/.vimrc
+if [ ! -d "$FILE" ]; then
+	echo "$FILE does not exist"
+fi
